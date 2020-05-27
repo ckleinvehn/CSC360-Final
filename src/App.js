2283577c02d802d58d953ced8969cdf0db7a4677
Christian Kleinvehn <ckleinvehn@gmail.com>
2020-05-27 13:58:12 -0500

    Brandon's work on the home page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import AppStyles from "./AppStyles"
 import Bar from "./BarChart";
 import Drawer from "@material-ui/core/Drawer";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import PopNews from "./PopNews"
 import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import React, { useState } from "react";
+import { positions } from '@material-ui/system';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,69 +19,8 @@ import {
 } from "react-router-dom";
 import "./App.css";
 
-const drawerWidth = 240;
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    backgroundColor: "orange",
-  },
-  logo: {
-    outline: "solid",
-    padding: "0.5rem",
-    borderRadius: "2rem",
-    "&:hover": { color: "lightgrey", transform: "scale(1.1)" },
-  },
-  barItems: {
-    paddingLeft: "1rem",
-  },
-  links: {
-    color: "white",
-    textDecoration: "none",
-    "&:hover": {
-      color: "lightgrey",
-      transform: "scale(1.3)",
-      textDecoration: "underline",
-    },
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  home: {
-    textAlign: "center",
-    animation: "slide",
-    animationDuration: "5s",
-    animationIterationCount: "1",
-    animationPlayState: "running",
-  },
-  "@keyframes slide": {
-    "0%": {
-      opacity: 0,
-    },
-    "50%": {
-      opacity: 0.5,
-    },
-    "100%": {
-      topacity: 1,
-    },
-  },
-}));
-
 function App() {
-  const classes = useStyles();
+  const classes = AppStyles();
 
   return (
     <div className={classes.root}>
@@ -89,7 +31,7 @@ function App() {
             <Typography className={classes.logo} variant="h6" noWrap>
               <Link className={classes.links} to="/">
                 {" "}
-                NYT - Archiver
+                News Flash
               </Link>
             </Typography>
             <Typography
@@ -128,21 +70,25 @@ function App() {
     </div>
   );
 }
-function Home() {
-  const classes = useStyles();
 
+
+
+function Home() {
+  const classes = AppStyles();
   return (
     <main className={classes.content}>
       <Toolbar />
-      <Typography paragraph>
-        <h1 className={classes.home}> WELCOME TO OUR PROJECT</h1>
+      <Typography variant="h3" align="center" gutterBottom>
+        <strong>CURRENT POPULAR NYTIMES ARTICLES</strong>
       </Typography>
+      <PopNews />
+      
     </main>
   );
 }
 
 function Welcome({ match, location }) {
-  const classes = useStyles();
+  const classes = AppStyles();
   return (
     <main className={classes.content}>
       <Toolbar />
@@ -153,7 +99,7 @@ function Welcome({ match, location }) {
   );
 }
 function Graph1() {
-  const classes = useStyles();
+  const classes = AppStyles();
   return (
     <main className={classes.content}>
       <Toolbar />
